594d434dcdbc69b6cda83fc0a5b578d07f82ad0b
Mark Vincent Baltes <kramsetlab99@gmail.com>
2025-10-28 19:01:43 +0800

    Update noteDao select query, add searchQuery on NoteViewModel, add 3 var and val searchQuery, isSearchActive and notes, Adding search view content

--- app/src/main/java/ph/edu/comteq/lesssoneleven/NoteViewModel.kt
+++ app/src/main/java/ph/edu/comteq/lesssoneleven/NoteViewModel.kt
@@ -4,12 +4,42 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 
 class NoteViewModel(application: Application) : AndroidViewModel(application) {
     private val noteDao: NoteDao = AppDatabase.getDatabase(application).noteDao()
 
-    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+    // Track what user is search for
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery
+
+    // show all notes OR notes that match the query
+//    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    val allNotes: Flow<List<Note>> = searchQuery.flatMapLatest { query ->
+        if (query.isEmpty()) { // or isBlack
+            noteDao.getAllNotes()    // show all notes
+        } else {
+            noteDao.searchNotes(query)  // show only notes that match the query
+        }
+    }
+
+    // call this when user types in the search query
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
+
+
+    // call this to clear search query
+    fun clearSearch() {
+        _searchQuery.value = ""
+    }
+
+
+
 
     fun insert(note: Note) = viewModelScope.launch {
         noteDao.insertNote(note)
@@ -22,4 +52,6 @@ class NoteViewModel(application: Application) : AndroidViewModel(application) {
     fun delete(note: Note) = viewModelScope.launch {
         noteDao.deleteNote(note)
     }
+
+
 }
